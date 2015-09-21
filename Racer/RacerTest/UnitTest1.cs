2188e0a9443ae3fbd5cc9bfcec12e9846da6708b
Drew Torgeson <drew.torgeson@gmail.com>
2015-09-20 22:00:00 -0600

    Initial Commit

---
+++ Racer/RacerTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RacerTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
